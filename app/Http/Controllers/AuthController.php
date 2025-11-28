5a07d131e68fa4c46aa1f6f89744bad3379a12bf
Andrey Repin <anrdaemon@yandex.ru>
2025-11-28 22:28:33 +0300

    Added auth controller and user seeder

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!auth()->attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        /** @var User */
+        $user = auth()->user();
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ]);
+    }
+}
